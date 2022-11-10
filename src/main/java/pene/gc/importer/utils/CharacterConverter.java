ec023ecebfe75936173de62ca4d408b911a351a9
Penelopeep <julek.zolich@gmail.com>
2022-11-10 18:54:51 +0100

    idk

--- src/main/java/pene/gc/importer/utils/CharacterConverter.java
+++ src/main/java/pene/gc/importer/utils/CharacterConverter.java
@@ -5,10 +5,16 @@ import com.google.gson.JsonElement;
 import emu.grasscutter.data.GameData;
 import emu.grasscutter.game.avatar.Avatar;
 import emu.grasscutter.game.player.Player;
+import pene.gc.importer.GenshinImporter;
 
 public class CharacterConverter {
     public static void main(JsonArray artifactCodes, Player targetPlayer) {
+        int count = 0;
+        int rarity = 0;
         for (JsonElement artifactCode : artifactCodes){
+            if (GenshinImporter.getPluginConfig().CharacterLimit != 0 && count == GenshinImporter.getPluginConfig().CharacterLimit) {
+                break;
+            }
             String avatarName = artifactCode.getAsJsonObject().get("location").getAsString();
             int avatarId = Datareader.getAvatarId(avatarName);
             if(targetPlayer.getAvatars().getAvatarById(avatarId)==null) {
@@ -18,7 +24,14 @@ public class CharacterConverter {
                 avatar.setPromoteLevel(1);
                 avatar.forceConstellationLevel(0);
                 avatar.recalcStats();
-                targetPlayer.getAvatars().addAvatar(avatar);
+                switch (avatar.getAvatarData().getQualityType()) {
+                    case "QUALITY_PURPLE" -> rarity = 4;
+                    case "QUALITY_ORANGE", "QUALITY_ORANGE_SP" -> rarity = 5;
+                }
+                if (GenshinImporter.getPluginConfig().CharacterRarity.contains(rarity)) {
+                    targetPlayer.getAvatars().addAvatar(avatar);
+                    count++;
+                }
             } else {
                 System.out.printf("Avatar %s already exists%n", avatarName);
             }
